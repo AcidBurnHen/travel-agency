122b099764ecc1af52349461f507e5983f9e0a17
Marin <cc.acidburn@gmail.com>
2022-04-01 17:15:00 +0200

    Configured Webpack to create bundled.js file

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,12 @@
+const path = require("path");
+// Importing path from the Node library
+
 module.exports = {
-  entry: "./app/assets/scripts/App.js"
+  entry: "./app/assets/scripts/App.js",
+  output: {
+    filename: "bundled.js",
+    // Generate absolute path to the correct folder
+    path: path.resolve(__dirname, "app")
+  },
+  mode: "development"
 };
