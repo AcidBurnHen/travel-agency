8fdf3083ef8ea6a0b31b7d625e87efc12bd12411
Marin <cc.acidburn@gmail.com>
2022-04-04 23:40:30 +0200

    Writing mobile first CSS with postcss-mixins

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 // Importing path from the Node library
 
-const postCSSPlugins = [require("postcss-import"), require("postcss-simple-vars"), require("postcss-nested"), require("autoprefixer")];
+const postCSSPlugins = [require("postcss-mixins"), require("postcss-import"), require("postcss-simple-vars"), require("postcss-nested"), require("autoprefixer")];
 
 module.exports = {
   entry: "./app/assets/scripts/App.js",
