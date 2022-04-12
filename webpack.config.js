84f31b9c0d041def4e53555948ccc0036f62133f
Marin <cc.acidburn@gmail.com>
2022-04-12 18:04:45 +0200

    Created a seperate dev and build enviroment

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,12 @@
-const path = require("path");
+const currentTask = process.env.npm_lifecycle_event;
+
 // Importing path from the Node library
+const path = require("path");
 
 const postCSSPlugins = [require("postcss-mixins"), require("postcss-import"), require("postcss-simple-vars"), require("postcss-nested"), require("autoprefixer")];
 
-module.exports = {
+let config = {
   entry: "./app/assets/scripts/App.js",
-  output: {
-    filename: "bundled.js",
-    // Generate absolute path to the correct folder
-    path: path.resolve(__dirname, "app")
-  },
-  devServer: {
-    before: function (app, server) {
-      server._watch("./app/**/*.html");
-    },
-    contentBase: path.join(__dirname, "app"),
-    hot: true,
-    port: 3000,
-    host: "0.0.0.0"
-  },
-  mode: "development",
   module: {
     rules: [
       {
@@ -30,3 +17,32 @@ module.exports = {
     ]
   }
 };
+
+if (currentTask == "dev") {
+  config.output = {
+    filename: "bundled.js",
+    // Generate absolute path to the correct folder
+    path: path.resolve(__dirname, "app")
+  };
+  config.devServer = {
+    before: function (app, server) {
+      server._watch("./app/**/*.html");
+    },
+    contentBase: path.join(__dirname, "app"),
+    hot: true,
+    port: 3000,
+    host: "0.0.0.0"
+  };
+  config.mode = "development";
+}
+
+if (currentTask == "build") {
+  config.output = {
+    filename: "bundled.js",
+    // Generate absolute path to the correct folder
+    path: path.resolve(__dirname, "dist")
+  };
+  config.mode = "production";
+}
+
+module.exports = config;
