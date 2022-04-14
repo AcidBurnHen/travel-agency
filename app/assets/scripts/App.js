8391e5b3c436f330b6e761637d5fc85376e530fd
Marin <cc.acidburn@gmail.com>
2022-04-14 20:31:40 +0200

    Added client area feautre

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -5,9 +5,11 @@ import "lazysizes";
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
+import ClientArea from "./modules/ClientArea";
 
 new StickyHeader();
 new MobileMenu();
+new ClientArea();
 new RevealOnScroll(document.querySelectorAll(".feature-item"));
 new RevealOnScroll(document.querySelectorAll(".testimonial"));
 let modal;
